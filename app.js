7f37fc3e4b16a51679918a2c216f382273fab564
Gunsy Dev <dharanisrivatsa@gmail.com>
2023-06-18 14:01:52 +0530

    Add SSL

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const https = require('https');
 const path = require('path');
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const { readFileSync } = require('fs');
+
+const sslServer = https.createServer(
+    {
+        key: readFileSync(path.join(__dirname, 'certs', 'key.pem')),
+        cert: readFileSync(path.join(__dirname, 'certs', 'cert.pem')),
+    },
+    app
+)
+sslServer.listen(3443, () => {
+    console.log('Secure Server is listening on port 3443');
 });
